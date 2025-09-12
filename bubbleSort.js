f4c5c5da870c26c62d87ef55ca8e6364720f0902
Abhishek Anil <abhishekanil551@gmail.com>
2025-09-12 08:48:09 +0530

    add bubble sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -13,3 +13,30 @@ function bubbleSort(arr) {
 
 let arr = [5, 3, 8, 4, 2];
 console.log("Sorted:", bubbleSort(arr));
+
+
+
+
+
+
+function bubbleSort(arr1) {
+    let swapped;
+    do {
+        swapped = false; // assume no swaps this round
+        for (let i = 0; i < arr1.length - 1; i++) {
+            if (arr1[i] > arr1[i + 1]) {
+                // swap
+                let temp = arr1[i];
+                arr1[i] = arr1[i + 1];
+                arr1[i + 1] = temp;
+                swapped = true; // swap happened, so need another pass
+            }
+        }
+    } while (swapped); // keep going until no swaps
+
+    return arr1;
+}
+
+// Example
+let arr1 = [5, 3, 8, 4, 2];
+console.log("Sorted:", bubbleSort(arr));
