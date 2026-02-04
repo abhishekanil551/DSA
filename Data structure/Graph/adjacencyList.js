857a666f4b38a0e8dd0cabd90a4e4af18d218f9c
Abhishek <abhishekanil551@gmail.com>
2026-02-04 05:38:38 +0200

    Graph (Adjacency List)

--- Data structure/Graph/adjacencyList.js
+++ Data structure/Graph/adjacencyList.js
@@ -8,28 +8,59 @@ class Graph{
         if(!this.adj.has(vertex)){
             this.adj.set(vertex,[])
         }
-    }
+    };
     
     addEdge(u , v , weight = 1){
         this.addvertex(u);
         this.addvertex(v);
-        this.adj.get(u).push([v , [weight] ]);
+        this.adj.get(u).push([v, weight]);
         if(!this.isDirected){
             this.adj.get(v).push([u , [weight]]);
         }
-    }
+    };
     
     getNeighbors(vertex){
         return this.adj.get(vertex) || [];
-    }
+    };
     
     print(){
         for(let [vertex,neighbors] of this.adj){
             console.log(`${vertex}->${neighbors.map(n => n[0]).join(', ')}`)
         }
-    }
+    };
     
-}
+    // DFS (Depth First Search)
+    dfs(start){
+        const visited = new Set();
+        const traverse = (vertex)=>{
+            if(visited.has(vertex)) return;
+            console(vertex);
+            visited.add(vertex);
+
+            for(let neighbors of this.adj.get(vertex)){
+                traverse(neighbors);
+            }
+        }
+        traverse(start);
+    };
+
+    // BFS (Breadth First Search)
+    bfs(start){
+        const visited = new Set();
+        const queue = [];
+        while(queue.length > 0){
+            const vertex = queue.shift();
+            console(vertex);
+            for(let neighbor of this.adj.get(vertex)){
+                if(!visited.has(neighbor)){
+                    visited.add(neighbor);
+                    queue.push(neighbor)
+                }
+            }
+        } 
+    };
+    
+};
 
 const g = new Graph(false); // undirected
 g.addEdge('A', 'B');
