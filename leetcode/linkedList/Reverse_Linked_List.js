c95065298ea1ca07317aad5ae8f921e3bd239f5f
Abhishek <abhishekanil551@gmail.com>
2026-02-06 06:10:02 +0200

    206. Reverse Linked List

---
+++ leetcode/linkedList/Reverse_Linked_List.js
@@ -0,0 +1,12 @@
+var reverseList = function(head) {
+    let current = head;
+    let prev = null;
+    let next = null;
+    while(current){
+        next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+    return prev;
+};
